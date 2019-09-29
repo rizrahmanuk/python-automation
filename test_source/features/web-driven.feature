4627a0faf1b949b79fad80820f7ea403356febe1
riz git user <rizrahmanuk@gmail.com>
2019-09-29 01:37:47 +0100

    Selenium-test-enhancements with XPath

--- test_source/features/web-driven.feature
+++ test_source/features/web-driven.feature
@@ -8,8 +8,13 @@ Feature: Home Office Web Browsing
   # One feature can have multiple scenarios
   # The lines immediately after the feature title are just comments
 
-  Scenario: Home Office Search
+  Scenario: Home Office Search for brexit
     Given the "http://homeoffice.gov.uk" page is displayed
     When the user searches for "brexit"
-    Then results are shown for "The UK is leaving the EU, find out how you should get ready for Brexit."
+    Then results are shown for "How to prepare if the UK leaves the EU with no deal"
     Then results are shown for "Visit Europe after Brexit"
+
+  Scenario: Home Office Search tax
+    Given the "http://homeoffice.gov.uk" page is displayed
+    When the user searches for "tax"
+    Then results are shown for "Income Tax rates and Personal Allowances"
--- test_source/step_defs/web_site_defs/web-test-def.py
+++ test_source/step_defs/web_site_defs/web-test-def.py
@@ -1,6 +1,11 @@
 import pytest
 from pytest_bdd import scenarios, given, when, then, parsers
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import NoAlertPresentException
+from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
 
@@ -16,11 +21,17 @@ scenarios('../../features/web-driven.feature')
 
 @pytest.fixture
 def browser():
-    firefox_bin=FirefoxBinary('/usr/bin')
-    b = webdriver.Firefox(executable_path='/usr/local/bin/geckodriver')
+    firefox_bin = FirefoxBinary('/usr/bin')
+    b: webdriver = webdriver.Firefox(executable_path='/usr/local/bin/geckodriver')
     b.implicitly_wait(20)
     yield b
     b.quit()
+
+
+@pytest.fixture
+def wait(browser):
+    w = WebDriverWait(browser, 20)
+    yield w
 
 
 # Given Steps
@@ -33,26 +44,38 @@ def go_to_site(browser, web_site):
 
 # When Steps
 @when(parsers.parse('the user searches for "{phrase}"'))
-def search_phrase(browser, phrase):
+def search_phrase(browser, wait, phrase):
     search_input = browser.find_element_by_id('site-search-text')
     search_input.send_keys(phrase + Keys.RETURN)
-    browser.implicitly_wait(10)
-
+    # search.submit()
 
 # Then Steps
 @then(parsers.parse('results are shown for "{phrase}"'))
-def search_results(browser, phrase):
-    #div =browser.find_element_by_id('js-results')
-    #paragraph_p=div.find_element_by_xpath('div/ol/li/p')
-    #print('\nparagraph_li='+str(paragraph_p.text))
+def search_results(browser, wait, phrase):
+    # div =browser.find_element_by_id('js-results')
+    # paragraph_p=div.find_element_by_xpath('div/ol/li/p')
+    # print('\nparagraph_li='+str(paragraph_p.text))
 
-    item_titles=browser.find_elements_by_class_name("gem-c-document-list__item-title")
+    # item_titles = wait.until(EC.presence_of_all_elements_located((By.CLASS_NAME, 'gem-c-document-list__item-title')))
+    wait.until(EC.presence_of_all_elements_located((By.CLASS_NAME, 'gem-c-document-list__item')))
 
-    for item_title in item_titles:
-        print('\nlist='+str(item_title.text))
+    # browser.find_element(by, hook)
+    # original XPath=/html/body/div[6]/main/div/form/div[2]/div/div[2]/div[4]/div/ol/li[1]
+    tag_a_list_of = wait.until(EC.presence_of_all_elements_located((By.XPATH,
+                                            '//*[@id="js-results"]/div/ol/li/a')))
 
+    for web_element in tag_a_list_of:
+        desc = web_element.text
+        print('\nDescription=' + str(desc))
+        if desc.__eq__(phrase):
+            return True
+    print('could not find expected result text=' + phrase)
+    assert False
+
+    # reference samples
     # assert str(paragraph_p.text) == phrase
-    #links_div = browser.find_element_by_id('links')
-    #assert len(links_div.find_elements_by_xpath('//div')) > 0
+    # link_tag=item.find_element(By.TAG_NAME, 'a')
+    # links_div = browser.find_element_by_id('links')
+    # assert len(links_div.find_elements_by_xpath('//div')) > 0
     ## search_input = browser.find_element_by_name('q')
-    #assert search_input.get_attribute('value') == phrase
+    # assert search_input.get_attribute('value') == phrase
